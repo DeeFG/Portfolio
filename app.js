a2e57b2d51c71337afabdf03c758403deaad3e48
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-05-17 22:59:30 -0400

    add js for sticky nav

--- app.js
+++ app.js
@@ -4,13 +4,33 @@
 window.onload = function () {
   $("#start").show();
   $(".threeCards").hide();
+  $("#navbar").hide();
 
   $(document).on("click", "#start", function () {
       $("#start").hide();
       $(".threeCards").show();
+      $("#navbar").show();
+      
   });
+
+  window.onscroll = function() {myFunction()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
+
 };
 
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
 });
+
+
