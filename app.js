bc7b1db60bd917a60e5e7a15f5b8ed1ef18cc242
DeeFG <xdeishax@gmail.com>
2019-09-02 06:32:06 -0400

    updated look of portfolio new bkg and transparent div

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 
 
 
-window.onload = function () {
-  $("#start").show();
-  $(".threeCards").hide();
-  $("#navbar").hide();
-  $("footer").show();
+// window.onload = function () {
+//   // $("#start").show();
+//   // $(".threeCards").hide();
+//   // $("#navbar").hide();
+//   // $("footer").show();
 
-  $(document).on("click", "#start", function () {
-      $("#start").hide();
-      $(".threeCards").show();
-      $("#navbar").show();
-      $("footer").show();
+//   // $(document).on("click", "#start", function () {
+//   //     // $("#start").hide();
+//   //     // $(".threeCards").show();
+//   //     // $("#navbar").show();
+//   //     // $("footer").show();
 
       
-  });
+//   // });
 
-  window.onscroll = function() {myFunction()};
+//   window.onscroll = function() {myFunction()};
 
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
+//   var navbar = document.getElementById("navbar");
+//   var sticky = navbar.offsetTop;
   
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
+//   function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//       navbar.classList.add("sticky")
+//     } else {
+//       navbar.classList.remove("sticky");
+//     }
+//   }
 
 
-};
+// };
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
-});
+// $(document).ready(function(){
+//   $('[data-toggle="tooltip"]').tooltip();   
+// });
 
 
